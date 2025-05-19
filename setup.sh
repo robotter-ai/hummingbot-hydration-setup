0115bdad4f93e857d88f54bbccb9627697f3fb4c
Danilo Araújo Silva <danilo.silva@funttastic.com>
2025-05-19 18:14:43 -0300

    Improving repository to the new name convention

--- setup.sh
+++ setup.sh
@@ -8,11 +8,11 @@ function init() {
 
 	# echo
 	# echo "Pulling latest docker images"
-	# docker pull funttastic/hydration_client
-	# docker pull funttastic/hydration_gateway
+	# docker pull funttastic/hummingbot:latest
+	# docker pull robotterlabs/gateway:latest
 
-	# Check if the hdx folder already exists in the current directory
-	if [ ! -d "hdx" ]; then
+	# Check if the volumes folder already exists in the current directory
+	if [ ! -d "volumes" ]; then
 		# Define a temporary directory for cloning
 		echo
 		TEMP_DIR=$(mktemp -d)
@@ -20,71 +20,71 @@ function init() {
 		git clone --depth 1 --branch feat/hydration --single-branch https://github.com/robotter-ai/hummingbot.git "$TEMP_DIR/hummingbot"
 
 		echo
-		echo "Creating hdx folder structure..."
+		echo "Creating volumes folder structure..."
 
-		mkdir -p hdx/common/certs
+		mkdir -p volumes/common/certs
 
-		# Create directories for hdx-client
-		mkdir -p hdx/client/conf
-		mkdir -p hdx/client/conf/connectors
-		mkdir -p hdx/client/conf/strategies
-		mkdir -p hdx/client/conf/controllers
-		mkdir -p hdx/client/conf/scripts
-		mkdir -p hdx/client/logs
-		mkdir -p hdx/client/data
-		mkdir -p hdx/client/scripts
-		mkdir -p hdx/client/controllers
+		# Create directories for hummingbot
+		mkdir -p volumes/client/conf
+		mkdir -p volumes/client/conf/connectors
+		mkdir -p volumes/client/conf/strategies
+		mkdir -p volumes/client/conf/controllers
+		mkdir -p volumes/client/conf/scripts
+		mkdir -p volumes/client/logs
+		mkdir -p volumes/client/data
+		mkdir -p volumes/client/scripts
+		mkdir -p volumes/client/controllers
 
-		# Create directories for hdx-gateway
-		mkdir -p hdx/gateway/conf
-		mkdir -p hdx/gateway/logs
+		# Create directories for gateway
+		mkdir -p volumes/gateway/conf
+		mkdir -p volumes/gateway/logs
 
 		echo
 		echo "Copying configuration folders from the cloned repository..."
-		# Copy folders from the cloned repo to hdx/client
-		cp -r "$TEMP_DIR/hummingbot/conf" hdx/client/
-		cp -r "$TEMP_DIR/hummingbot/scripts" hdx/client/
-		cp -r "$TEMP_DIR/hummingbot/controllers" hdx/client/
+		# Copy folders from the cloned repo to volumes/client
+		cp -r "$TEMP_DIR/hummingbot/conf" volumes/client/
+		cp -r "$TEMP_DIR/hummingbot/scripts" volumes/client/
+		cp -r "$TEMP_DIR/hummingbot/controllers" volumes/client/
 
-		chmod -R 777 hdx
+		chmod -R 777 volumes
 
 		echo "Folder structure and files copied."
 	else
-		echo "Folder 'hdx' already exists. Skipping repository copy."
+		echo "Folder 'volumes' already exists. Skipping repository copy."
 	fi
 
 	# Clean up the temporary directory
 	rm -rf "$TEMP_DIR"
 }
 
-function create_hdx_client() {
+function create_hummingbot() {
 	echo
-	echo "Starting hdx-client container..."
-	GATEWAY_PASSPHRASE="" docker compose up -d hdx-client
+	echo "Starting hummingbot container..."
+	GATEWAY_PASSPHRASE="" docker compose up -d hummingbot
 }
 
-function create_hdx_gateway() {
+function create_gateway() {
 	echo
-	echo "Starting hdx-gateway container..."
+	echo "Starting gateway container..."
 	echo
 	echo "If you have already generated your GATEWAY_PASSPHRASE using the 'gateway generate-certs' command inside of the Hummingbot Client, enter it below:"
 	read -s -p "Enter your gateway passphrase: " gateway_passphrase
 	echo
-	GATEWAY_PASSPHRASE="$gateway_passphrase" docker compose up -d hdx-gateway
-	docker logs -f hdx-gateway
+	GATEWAY_PASSPHRASE="$gateway_passphrase" docker compose up -d gateway
+	docker logs -f gateway
 }
 
 # Main logic to handle script arguments
 case "$1" in
-	hdx-client)
+	hummingbot)
 		init
-		create_hdx_client
+		create_hummingbot
 		;;
-	hdx-gateway)
-		create_hdx_gateway
+	gateway)
+		create_gateway
 		;;
 	*)
-		echo "Usage: $0 {hdx-client|hdx-gateway}"
+		echo "Usage: $0 {hummingbot|gateway}"
 		exit 1
 		;;
 esac
